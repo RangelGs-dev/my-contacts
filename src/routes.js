f55c2de81fd0c3670cc5e75c9b18257163b65e80
Rangel Sousa <rangelgs17@gmail.com>
2025-03-28 12:51:36 -0300

    Mais rotas e repository

--- src/routes.js
+++ src/routes.js
@@ -7,5 +7,8 @@ const router = Router();
 router.get("/contacts", ContactController.index);
 router.get("/contacts/:id", ContactController.show);
 router.delete("/contacts/:id", ContactController.delete);
+router.post("/contacts", ContactController.store);
+router.put("/contacts/:id", ContactController.update);
+// O metodo .use também é usado para as rotas, pois as mesmas são também entendidos como middlewares
 
 module.exports = router;
